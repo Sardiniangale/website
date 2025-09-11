24557b89731f770d8f61eeed0cf104c846a3bf57
Giacomo Solinas <sardiniangale24@gmail.com>
2025-09-11 19:09:26 +0200

    t

--- functions/turnstile.js
+++ functions/turnstile.js
@@ -1,20 +1,5 @@
-
 export async function onRequest(context) {
-  const { request, env } = context;
-  const { token } = await request.json();
-
-  const formData = new FormData();
-  formData.append('secret', env.TURNSTILE_SECRET_KEY);
-  formData.append('response', token);
-
-  const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-    method: 'POST',
-    body: formData,
-  });
-
-  const data = await response.json();
-
-  return new Response(JSON.stringify(data), {
+  return new Response(JSON.stringify({ success: true, message: "Hello from the turnstile function!" }), {
     headers: { 'Content-Type': 'application/json' },
   });
 }
